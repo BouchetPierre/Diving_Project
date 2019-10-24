b6a925e55f58ee935eb08e6b0418300edce54689
BouchetPierre <bouchet.hp@gmail.com>
2019-10-24 17:38:15 +0200

    Base de donnees

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,4 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-];
+    ];
